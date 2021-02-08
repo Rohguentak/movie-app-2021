cee5630df739dbae74b7a3b31fb47b51e754e472
Rohguentak <rhgt78@gmail.com>
2021-02-08 15:59:28 +0900

    concept practice before make movie app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import PropTypes from "prop-types"
+import { render } from '@testing-library/react'
 
-function App() {
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    console.log("hi")
+  }
+  state = {
+    count:0
+  };
+  add=() => {
+    //this.setState({count:1})
+    //this.setState({count: this.state.count +1})
+    this.setState(current => ({count: current.count +1}))
+  };
+  delete = () => {
+    //this.setState({count:-1})
+    this.setState({count:this.state.count -1})
+  };
+  render() {
+    return (
+      <div>
+        <h1>hello class {this.state.count} component</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.delete}>delete</button>
+      </div>
+    );
+    
+  }
+}
+/*function Food({fav,picture,rating}){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>i like {fav}</h1>
+      <h4>rating : {rating}/10</h4>
+      <img src={picture} />
     </div>
   );
-}
+}*/
+
+/*Food.propTypes = {
+  fav: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}*/
+
+//const foodlike = [{id:1,name: "potato",image:"http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",rating: 5},
+//{id:2,name:"melon",image:"https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",rating: 6}];
+
+//function renderFood(dish){
+//  return <Food fav={dish.name} picture={dish.image}/>
+//}
+
+/*function App() {
+  return (
+//    <div className="App">
+//      {foodlike.map(renderFood)}
+//    </div>
+    <div>
+      {foodlike.map(dish => (<Food rating={dish.rating} key={dish.id} fav={dish.name} picture={dish.image}/>))}
+    </div>
+  );
+}*/
 
 export default App;
