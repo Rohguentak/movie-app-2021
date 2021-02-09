231f0433d721884a20432795ef4a713309b7c4be
rohguentak <rhgt78@gmail.com>
2021-02-09 17:13:45 +0900

    add nav link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./navigation.css";
 function Navigation() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+    <div className="nav">
+      <Link to="/" className="home">
+        Home
+      </Link>
+      <Link to="/about" className="about">
+        About
+      </Link>
     </div>
   );
 }
