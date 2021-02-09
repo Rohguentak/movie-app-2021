03cdbcde5188ac9fd17d030491e54fd7165b4122
Rohguentak <52411864+Rohguentak@users.noreply.github.com>
2021-02-09 13:36:57 +0900

    add genres info

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
-import React from 'react';
-import axios from 'axios';
-import Movies from './movies'
-import "./app.css"
-class App extends React.Component{
+import React from "react";
+import axios from "axios";
+import Movies from "./movies";
+import "./app.css";
+class App extends React.Component {
   state = {
     isLoading: true,
-    movies: []
-  }
-  getMovies = async() => {
-    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
-    this.setState({movies,isLoading: false}) // 아래와 같은 의미
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false }); // 아래와 같은 의미
     //this.setState({movies:movies}) //앞의 movies는 state의 배열 뒤의 movies는 axios로 받아온 movies
+  };
+  componentDidMount() {
+    this.getMovies();
   }
-  componentDidMount(){
-    this.getMovies()
-  }
-  render(){
-    const {isLoading,movies} = this.state
-    return (<section class="container">
-      {isLoading ? (
-        <div class="loader">
-          <span class="loader__text">Loading...</span>
-        </div>
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
         ) : (
-          <div class="movies">
-            {movies.map(movie => (
-              <Movies 
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movies
                 key={movie.id}
                 id={movie.id}
                 year={movie.year}
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
-              />)
-          )}
+                genres={movie.genres}
+              />
+            ))}
           </div>
         )}
-    </section>
-    )
+      </section>
+    );
   }
 }
 
--- src/movies.js
+++ src/movies.js
@@ -1,26 +1,35 @@
-import React from "react"
-import PropTypes from "prop-types"
-import "./movie.css"
-function Movies({id, year,title,summary,poster}){
-    return <div class="movie">
-        <div class="movie_poster">
-            <img src={poster} title={title} alt={title}/>
-        </div>
-        <div class="movie_info">
-            <h3 class="movie_title">{title}</h3>
-            <h5 class="movie_year">{year}</h5>
-            <p class="movie_suppary">{summary}</p>
-        </div>
+import React from "react";
+import PropTypes from "prop-types";
+import "./movie.css";
+function Movies({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <div className="movie_poster">
+        <img src={poster} title={title} alt={title} />
+      </div>
+      <div className="movie_info">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li className="genres__genre" key={index}>
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie_suppary">{summary}</p>
+      </div>
     </div>
+  );
 }
 
 Movies.propTypes = {
-    id: PropTypes.number.isRequired,
-    year: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-}
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
-
-export default Movies
+export default Movies;
